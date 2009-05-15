1239601e4a6ac183cf4059ac17e1bf55e10bed98
mikesamuel@gmail.com <mikesamuel@gmail.com@ff039bb0-f41e-0410-b2c7-0399d50afb14>
2009-05-15 17:48:25 +0000

    Andrew Pennebaker's patch for hex literal support in lisp mode.

--- src/lang-lisp.js
+++ src/lang-lisp.js
@@ -79,7 +79,7 @@ PR.registerLangHandler(
         [
          [PR.PR_KEYWORD,     /^(?:block|c[ad]+r|catch|cons|defun|do|eq|eql|equal|equalp|eval-when|flet|format|go|if|labels|lambda|let|load-time-value|locally|macrolet|multiple-value-call|nil|progn|progv|quote|require|return-from|setq|symbol-macrolet|t|tagbody|the|throw|unwind)\b/, null],
          [PR.PR_LITERAL,
-          /^[+\-]?(?:\d+\/\d+|(?:\.\d+|\d+(?:\.\d*)?)(?:[eEdD][+\-]?\d+)?)/],
+          /^[+\-]?(?:0x[0-9a-f]+|\d+\/\d+|(?:\.\d+|\d+(?:\.\d*)?)(?:[ed][+\-]?\d+)?)/i],
          // A single quote possibly followed by a word that optionally ends with
          // = ! or ?.
          [PR.PR_LITERAL,
