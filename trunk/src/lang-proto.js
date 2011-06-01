4445e404c574ac5390412414efe4cac1c926d237
mikesamuel@gmail.com <mikesamuel@gmail.com@ff039bb0-f41e-0410-b2c7-0399d50afb14>
2011-06-01 23:29:22 +0000

    Fix proto lang handler to work minified and to use the type style for types like uint32.

--- trunk/src/lang-proto.js
+++ trunk/src/lang-proto.js
@@ -25,11 +25,11 @@
  */
 
 PR['registerLangHandler'](PR['sourceDecorator']({
-        keywords: (
-            'bool bytes default double enum extend extensions false fixed32 '
-            + 'fixed64 float group import int32 int64 max message option '
-            + 'optional package repeated required returns rpc service '
-            + 'sfixed32 sfixed64 sint32 sint64 string syntax to true uint32 '
-            + 'uint64'),
-        cStyleComments: true
+        'keywords': (
+            'bytes,default,double,enum,extend,extensions,false,'
+            + 'group,import,max,message,option,'
+            + 'optional,package,repeated,required,returns,rpc,service,'
+            + 'syntax,to,true'),
+        'types': /^(bool|(double|s?fixed|[su]?int)(32|64)|float|string)\b/,
+        'cStyleComments': true
       }), ['proto']);
