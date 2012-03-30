0603e01594263a36744a79064d6448111c999d14
mikesamuel@gmail.com <mikesamuel@gmail.com@ff039bb0-f41e-0410-b2c7-0399d50afb14>
2012-03-30 18:45:11 +0000

    Fixed issue 150 : "let" missing from C# keyword list

--- trunk/js-modules/prettify.js
+++ trunk/js-modules/prettify.js
@@ -83,10 +83,10 @@ window['PR_SHOULD_USE_CONTINUATION'] = true;
       "transient"];
   var CSHARP_KEYWORDS = [JAVA_KEYWORDS,
       "as,base,by,checked,decimal,delegate,descending,dynamic,event," +
-      "fixed,foreach,from,group,implicit,in,interface,internal,into,is,lock," +
-      "object,out,override,orderby,params,partial,readonly,ref,sbyte,sealed," +
-      "stackalloc,string,select,uint,ulong,unchecked,unsafe,ushort,var," +
-      "virtual,where"];
+      "fixed,foreach,from,group,implicit,in,interface,internal,into,is,let," +
+      "lock,object,out,override,orderby,params,partial,readonly,ref,sbyte," +
+      "sealed,stackalloc,string,select,uint,ulong,unchecked,unsafe,ushort," +
+      "var,virtual,where"];
   var COFFEE_KEYWORDS = "all,and,by,catch,class,else,extends,false,finally," +
       "for,if,in,is,isnt,loop,new,no,not,null,of,off,on,or,return,super,then," +
       "throw,true,try,unless,until,when,while,yes";
