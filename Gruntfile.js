99aefbee79e1d598baab51437cb67a84ff6df8a3
Amro <amroamroamro@gmail.com>
2017-01-07 09:03:25 +0200

    add grunt as dependency and create initial Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+/**
+ * google-code-prettify
+ * https://github.com/google/code-prettify
+ *
+ * Copyright (C) 2017 Google Inc.
+ * Licensed under Apache 2.0 license.
+ */
+
+module.exports = function (grunt) {
+  'use strict';
+
+  // project configuration
+  grunt.initConfig({
+    // metadata
+    pkg: grunt.file.readJSON('package.json')
+  });
+
+  // register task aliases
+  grunt.registerTask('default', []);
+};
