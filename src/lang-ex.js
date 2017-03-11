c078ac69eadf74b0d52267cbc6ca8fd6b6d7e1e4
Jacek Królikowski <nietaki@gmail.com>
2017-03-11 13:19:22 +0000

    Elixir: fixing the license text.

--- src/lang-ex.js
+++ src/lang-ex.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (C) 2017 Google Inc.
+ * Copyright (C) 2017 Jacek Królikowski
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
