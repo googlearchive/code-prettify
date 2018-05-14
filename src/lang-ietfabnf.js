2606f27f03f0d5a60466b389b85fbf9983ce6d9a
Julian Reschke <julian.reschke@gmx.de>
2018-05-14 19:47:46 +0100

    fix date

--- src/lang-ietfabnf.js
+++ src/lang-ietfabnf.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (C) 2008 Julian Reschke
+ * Copyright (C) 2018 Julian Reschke
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
