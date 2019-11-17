050b9681a20fc7d0576d13ce7e83b0a3c7936f44
mgr <mgrojo@gmail.com>
2019-11-17 01:01:55 +0100

    Update Ada support
    - Added to README
    - Updated header
    - Added Ada source code to test
    
    Fixes issue #312

--- src/lang-ada.js
+++ src/lang-ada.js
@@ -1,8 +1,28 @@
+/**
+ * @license
+ * Copyright (C) 2015 Felix Krause
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // Contributed by Felix Krause <contact curl flyx dot org>
 
 /**
  * @fileoverview
  * Registers a language handler for Ada 2012.
+ * To use, include prettify.js and this file in your HTML page.
+ * Then put your code in an HTML tag like
+ *      <pre class="prettyprint lang-ada">(my Ada code)</pre>
  *
  * @author Felix Krause <contact curl flyx dot org>
  */
